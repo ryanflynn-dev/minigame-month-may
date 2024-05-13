23a4b53447205a8916179d148b87d07ae3994f8d
Ryan Flynn <122565977+ryanflynn-dev@users.noreply.github.com>
2024-05-13 13:52:18 +0100

    Prettier formatting

--- js/core/control.js
+++ js/core/control.js
@@ -3,11 +3,11 @@
  * @typedef {{left: boolean, up: boolean, right: boolean, down: boolean}} keyCodes
  */
 const controls = {
-    left: false,
-    up: false,
-    right: false,
-    down: false,
-}
+  left: false,
+  up: false,
+  right: false,
+  down: false,
+};
 
 /**
  * Handles control input and updates controls object
@@ -18,44 +18,46 @@ const controls = {
  *
  */
 const handleControlInput = (e, toggle) => {
-    e.preventDefault();
-    switch (e.code) {
-      case "ArrowLeft": 
-      case "KeyA": 
-        controls.left = toggle;
-        break;
-      case "ArrowRight": 
-      case "KeyD": 
-        controls.right = toggle;
-        break;
-      case "ArrowUp": 
-      case "KeyW": 
-        controls.up = toggle;
-        break;
-      case "ArrowDown": 
-      case "KeyS": 
-        controls.down = toggle;
-        break;
-    }
-}
-
+  e.preventDefault();
+  switch (e.code) {
+    case "ArrowLeft":
+    case "KeyA":
+      controls.left = toggle;
+      break;
+    case "ArrowRight":
+    case "KeyD":
+      controls.right = toggle;
+      break;
+    case "ArrowUp":
+    case "KeyW":
+      controls.up = toggle;
+      break;
+    case "ArrowDown":
+    case "KeyS":
+      controls.down = toggle;
+      break;
+  }
+};
 
 /**
- * Initialise keyboard controls. 
- * 
+ * Initialise keyboard controls.
+ *
  * @return null
  */
 export const initControls = () => {
-    document.addEventListener("keydown", (e) => {handleControlInput(e, true)});
-    document.addEventListener("keyup", (e) => {handleControlInput(e, false)});
-}
+  document.addEventListener("keydown", (e) => {
+    handleControlInput(e, true);
+  });
+  document.addEventListener("keyup", (e) => {
+    handleControlInput(e, false);
+  });
+};
 
 /**
- * Gets controls object 
- * 
+ * Gets controls object
+ *
  * @return Object.controls
  */
 export const getControls = () => {
-    return controls;
-}
-
+  return controls;
+};
